30cca2d31afb5e90496b9fdd15c744eb2f99fb6f
Anna Jaunzeme <anna.jaunzeme@inbox.lv>
2021-10-21 18:29:50 +0300

    homework

---
+++ Homework.4_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework._4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] myArray = new int[] { 5, 6, 9, 56, 745, 45, 6, 9, 87, 62, 3456, 7890, 12, 34, 5678, 987, 65, 43, 223, 4, 5, 636};
+            int sum = myArray.Sum();
+            Console.WriteLine("The Sum  of this Array is : " + sum);
+            Console.ReadLine();
+           
+            // ...
+           
+        }
+    }
+}
+ 
